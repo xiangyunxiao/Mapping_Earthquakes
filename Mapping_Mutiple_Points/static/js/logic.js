738bf991ee7b1fc60b6fcb34f9e456ff94b26a11
Yunxiao Xiang <yxiang@drrt.com>
2022-02-22 16:09:04 -0500

    mutiple city and line on map

--- Mapping_Mutiple_Points/static/js/logic.js
+++ Mapping_Mutiple_Points/static/js/logic.js
@@ -3,12 +3,23 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
-    radius: 30000,
-    color: "black",
-    fillColor: "ffffa1"
- }).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+//cityData.forEach(function(city) {
+  //  console.log(city)
+    // L.marker(city.location).addTo(map);
+// });
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {radius: city.population/200000, color: "orange", fillColor: "ffffa1", background: "black"})
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
