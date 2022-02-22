7baf80f1093ac6bdab19871f914b172261baf7c0
Yunxiao Xiang <yxiang@drrt.com>
2022-02-22 14:58:51 -0500

    single marker

--- Mapping_Mutiple_Points/static/js/logic.js
+++ Mapping_Mutiple_Points/static/js/logic.js
@@ -5,8 +5,8 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
 L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: "orange",
+    radius: 30000,
+    color: "black",
     fillColor: "ffffa1"
  }).addTo(map);
 
